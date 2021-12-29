f1e7b84ca16ad770313a4cc0da816d69a11d2a38
LankyDan <danknewton@hotmail.com>
2021-12-29 21:46:14 +0000

    Add `family` to `Person` schema type

--- src/main/kotlin/dev/lankydan/people/data/RelationshipRepository.kt
+++ src/main/kotlin/dev/lankydan/people/data/RelationshipRepository.kt
@@ -8,4 +8,12 @@ interface RelationshipRepository : CrudRepository<Relationship, UUID> {
 
   @Query("SELECT r FROM Relationship r LEFT JOIN FETCH r.relatedPerson p WHERE r.personId = :personId")
   fun findAllByPersonId(personId: UUID): List<Relationship>
+
+  @Query(
+    "SELECT r FROM Relationship r " +
+        "LEFT JOIN FETCH r.relatedPerson p " +
+        "WHERE r.personId = :personId " +
+        "AND r.relationship IN :relationships"
+  )
+  fun findAllByPersonIdAndInRelationships(personId: UUID, relationships: Set<String>): List<Relationship>
 }
---
+++ src/main/kotlin/dev/lankydan/people/graphql/fetchers/PersonFamilyDataFetcher.kt
@@ -0,0 +1,29 @@
+package dev.lankydan.people.graphql.fetchers
+
+import dev.lankydan.people.data.RelationshipRepository
+import dev.lankydan.people.graphql.TypedDataFetcher
+import dev.lankydan.people.graphql.schema.dtos.PersonDTO
+import dev.lankydan.people.graphql.schema.dtos.RelationshipDTO
+import dev.lankydan.people.graphql.schema.dtos.toDTO
+import graphql.schema.DataFetchingEnvironment
+import org.springframework.stereotype.Component
+
+@Component
+class PersonFamilyDataFetcher(
+  private val relationshipRepository: RelationshipRepository
+) : TypedDataFetcher<List<RelationshipDTO>> {
+
+  private companion object {
+    val family = setOf("Husband", "Wife", "Brother", "Sister", "Father", "Mother")
+  }
+
+  override val typeName = "Person"
+  override val fieldName = "family"
+
+  override fun get(environment: DataFetchingEnvironment): List<RelationshipDTO> {
+    val source = environment.getSource<PersonDTO>()
+    return source.relationships.ifEmpty {
+      relationshipRepository.findAllByPersonIdAndInRelationships(source.id, family).map { it.toDTO() }
+    }
+  }
+}
